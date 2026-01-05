a1cd25ce2d04bb51c04002b23de62b7a5458b8cb
surya s nair <suryaschempakathil03@gmail.com>
2026-01-05 13:12:20 +0530

    encapsulation assignment completed

--- Java OOPs encapsulation Assignment/Assignment_4/Product.java
+++ Java OOPs encapsulation Assignment/Assignment_4/Product.java
@@ -4,7 +4,16 @@ public class Product {
     private int price;
 
     public void setPrice(int price) {
+        if(price >= 100 && price <= 100000){
         this.price = price;
+
+        }else{
+            System.out.println("Invalid price.price must be between 100-100000");
+        }
+    }
+
+    public int getPrice() {
+        return price;
     }
     
 }
--- Java OOPs encapsulation Assignment/Assignment_4/ProductTest.java
+++ Java OOPs encapsulation Assignment/Assignment_4/ProductTest.java
@@ -1,5 +1,10 @@
 package Assignment_4;
 
 public class ProductTest {
-    
+    public static void main(String[] args) {
+        Product p=new Product();
+        p.setPrice(1000);
+       System.out.println("product price:"+p.getPrice()); 
+    }
+
 }
